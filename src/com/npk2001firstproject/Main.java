7636556fd52b5b56e9588867053b5cfc0ee6c03c
npk2001 <91059332+npk2001@users.noreply.github.com>
2021-09-27 21:41:51 +0100

    Initial commit

---
+++ src/com/npk2001firstproject/Main.java
@@ -0,0 +1,9 @@
+package com.npk2001firstproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World This is my first java programme");
+	// write your code here
+    }
+}
